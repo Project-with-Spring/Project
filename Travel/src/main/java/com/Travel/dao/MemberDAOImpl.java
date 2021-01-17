21261c5f855af64c510018b20099b5bc9545029a
eee269 <skfkd3123@gmail.com>
2021-01-18 00:11:57 +0900

    Spring Setting

--- Travel/src/main/java/com/Travel/dao/MemberDAOImpl.java
+++ Travel/src/main/java/com/Travel/dao/MemberDAOImpl.java
@@ -11,6 +11,6 @@ public class MemberDAOImpl implements MemberDAO {
 	//mybatis 생성자 통해서 객체생성 
 		@Inject
 		private SqlSession sqlSession;
-		private static final String namespace="com.Travel.go.mapper.MemberMapper";
+		private static final String namespace="com.Travel.mapper.MemberMapper";
 
 }
--- Travel/src/main/webapp/WEB-INF/views/home.jsp
+++ Travel/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,7 +1,7 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
-<html>
+<!DOCTYPE html>
+<html xmlns="http://www.thymeleaf.org">
 <head>
+	<meta charset="UTF-8">
 	<title>Home</title>
 </head>
 <body>
