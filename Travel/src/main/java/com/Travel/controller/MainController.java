6d1964eeb272183e20656246d8ad5b531aac5c7f
seo <seo@218.38.137.27>
2021-01-20 23:26:06 +0900

    jsp 확

---
+++ Travel/src/main/java/com/Travel/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.Travel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	
+
+	//http://localhost:8080/go/main　　
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String main() {
+//		/WEB-INF/views/main/main.jsp
+		return "main/main";
+	}
+	
+	//http://localhost:8080/go/login　　
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+//		/WEB-INF/views/main/login.jsp
+		return "main/login";
+	}
+	
+	
+}
---
+++ Travel/src/main/java/com/Travel/controller/SaleController.java
@@ -0,0 +1,17 @@
+package com.Travel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SaleController {
+	
+	//http://localhost:8080/go/sale　　
+	@RequestMapping(value = "/sale", method = RequestMethod.GET)
+	public String sale() {
+//		/WEB-INF/views/main/login.jsp
+		return "sub1/sale";
+	}
+	
+}
