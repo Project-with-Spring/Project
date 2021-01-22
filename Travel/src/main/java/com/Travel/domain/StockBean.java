913db4b10d432c3769b8c0fb1f1cffdfe895a492
eee269 <skfkd3123@gmail.com>
2021-01-23 01:38:29 +0900

    lombok 적용

--- Travel/src/main/java/com/Travel/domain/StockBean.java
+++ Travel/src/main/java/com/Travel/domain/StockBean.java
@@ -3,15 +3,16 @@ package com.Travel.domain;
 import java.sql.Timestamp;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-
-@Data
+@Getter
+@Setter
 public class StockBean {
-	private Long stc_id;
-	private String stc_name;
-	private int stc_count;
-	private int stc_cost;
-	private Timestamp stc_in;
-	private String ctg_id;
-	
+	private Long stc_id;		// 재고 id
+	private String stc_name;	// 재고 이름
+	private int stc_cost;		// 재고 가격
+	private int stc_count;		// 재고 수량
+	private Timestamp stc_in;	// 입고 날짜
+	private String ctg_id;		// 카테고리 id
 }
