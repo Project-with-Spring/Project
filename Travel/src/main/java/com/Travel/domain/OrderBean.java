b0bee971781c48afd7f3e728b738bad9c821d8be
gkdl6975 <gkdl6975@naver.com>
2021-01-26 16:29:29 +0900

    2021-01-26 16:27 sale.js 생성 / Order, OrderDetail 생성/ lombok적용 판매 -> DB 적용 확인완료

---
+++ Travel/src/main/java/com/Travel/domain/OrderBean.java
@@ -0,0 +1,19 @@
+package com.Travel.domain;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderBean {
+	private String ord_id;
+	private String ord_discount;
+	private String ord_total;
+	private String ord_cancel;
+	private String pmt_name;
+	private String pot_id;
+	private Timestamp ord_date;
+	
+}
---
+++ Travel/src/main/java/com/Travel/domain/OrderDetailBean.java
@@ -0,0 +1,14 @@
+package com.Travel.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderDetailBean {
+	private String odt_id;
+	private String odt_count;
+	private String ord_id;
+	private String pdt_id;
+	private String odt_memo;
+}
